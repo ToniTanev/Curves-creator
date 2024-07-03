b1b5acc4a24a7c1eef32f0f37365e1a516b8273d
ToniTanev <toni.g.tanev@gmail.com>
2024-07-04 00:59:08 +0300

    Adding BezierCurve class

--- Math.js
+++ Math.js
@@ -1,6 +1,44 @@
-function Slerp(u, v, t)
+function slerp(u, v, t)
 {
     const angleBetween = u.angleTo(v);
 
     return (Math.sin(angleBetween * (1 - t)) * u + Math.sin(angleBetween * t) * v) / Math.sin(angleBetween);
+}
+
+class BezierCurve
+{
+    constructor(controlPoints)
+    {
+        this.controlPoints = controlPoints
+        this.n = controlPoints.size;
+    }
+
+    generateCurve()
+    {
+        const curvePoints = [];
+        for(let t = 0.0; t <= 1.0; t += 0.01)
+        {
+            curvePoints.push(this.generatePoint(t));
+        }
+
+        return curvePoints;
+    }
+    generatePoint(t)
+    {
+        const steps = this.n - 1;
+        let prevPoints = this.controlPoints;
+
+        for(let i = 0; i < steps; i++)
+        {
+            const currPoints = [];
+            for(let j = 0; j < prevPoints.size - 1; j++)
+            {
+                currPoints.push(slerp(prevPoints[j], prevPoints[j + 1], t));
+            }
+
+            prevPoints = currPoints;
+        }
+
+        return prevPoints[0];
+    }
 }
