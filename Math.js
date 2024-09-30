1491afad05d69444ff73211eb8b2f56a7af0d200
ToniTanev <toni.g.tanev@gmail.com>
2024-09-30 23:28:21 +0300

    Implementing the strans function and the cubic hermite curve class

--- Math.js
+++ Math.js
@@ -13,6 +13,20 @@ function slerp( u, v, t )
     return nom.divideScalar( Math.sin( angleBetween ) );
 }
 
+function strans(u, v)
+{
+    // copy vectors, so that we don't modify the originals
+    const u1 = u.clone();
+    const v1 = v.clone();
+
+    const uLen = u1.length();
+    const vLen = v1.length();
+
+    const vScaled = v1.normalize().multiplyScalar( uLen );
+
+    return u1.multiplyScalar( Math.cos( vLen ) ).add( vScaled.multiplyScalar( Math.sin( vLen ) ) );
+}
+
 const epsilonOffset = 0.07;
 export class BezierCurve
 {
@@ -48,6 +62,80 @@ export class BezierCurve
         }
 
         return prevPoints[ 0 ];
+    }
+}
+
+class CubicHermiteCurve
+{
+    constructor( p0, v0, p1, v1 )
+    {
+        this.p0 = p0;
+        this.v0 = v0;
+        this.p1 = p1;
+        this.v1 = v1;
+    }
+
+    generateCurve()
+    {
+        const curvePoints = [];
+        for( let t = 0.0; t <= 1.0; t += 0.01 )
+        {
+            curvePoints.push( this.generatePoint( t ) );
+        }
+
+        return curvePoints;
+    }
+    generatePoint( t )
+    {
+        let pt = slerp( this.p0, this.p1, this.H1( t ) );
+        pt = strans( pt, this.v0.multiplyScalar( this.h0( t ) ) );
+        pt = strans( pt, this.v1.multiplyScalar( this.h1( t ) ) );
+
+        return pt;
+    }
+
+    H1( t )
+    {
+        return Math.pow( t, 2 ) * ( 3 - 2*t );
+    }
+
+    h0( t )
+    {
+        return t * Math.pow( ( 1 - t ), 2 );
+    }
+
+    h1( t )
+    {
+        return Math.pow( t, 2 ) * ( t - 1 );
+    }
+}
+
+export class CubicHermiteCurves
+{
+    constructor( controlPoints, controlVectors )
+    {
+        this.controlPoints = controlPoints;
+        this.controlVectors = controlVectors;
+    }
+
+    generateCurves()
+    {
+        const allPoints = [];
+
+        for( let i = 0; i < this.controlPoints.length - 1; i += 2 )
+        {
+            const p0 = this.controlPoints[ i ];
+            const v0 = this.controlVectors[ i ];
+            const p1 = this.controlPoints[ i + 1 ];
+            const v1 = this.controlVectors[ i + 1 ];
+
+            const twoPointCurve = new CubicHermiteCurve(p0, v0, p1, v1);
+            const currPoints = twoPointCurve.generateCurve();
+
+            allPoints.push( ...currPoints );
+        }
+
+        return allPoints;
     }
 }
 
