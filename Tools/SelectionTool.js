1ea984942213a4899546074a9d47c8cc93d58169
ToniTanev <toni.g.tanev@gmail.com>
2024-12-29 18:11:20 +0200

    Adding the Selection tool skeleton

---
+++ Tools/SelectionTool.js
@@ -0,0 +1,34 @@
+
+
+export class SelectionTool
+{
+    constructor()
+    {
+        this.clear();
+    }
+
+    clear()
+    {
+        this.selectedObj = null;
+    }
+
+    pointAdded( mouse )
+    {
+
+    }
+
+    onInteractive( mouse )
+    {
+
+    }
+
+    complete()
+    {
+
+    }
+
+    revert()
+    {
+
+    }
+}
