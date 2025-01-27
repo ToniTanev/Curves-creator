eb05107a09d0f46f0ad7cd9608449dc8c6785303
ToniTanev <toni.g.tanev@gmail.com>
2025-01-27 02:23:57 +0200

    Showing/hiding tool settings

--- UI/UIHandler.js
+++ UI/UIHandler.js
@@ -10,6 +10,42 @@ export const ToolIDs = Object.freeze({
 const defaultButtonColor = "#008CBA";
 const activeButtonColor = "rgb(0, 200, 250)";
 
+function showToolSettings( toolID, show = true )
+{
+    const visibility = show ? "visible" : "hidden";
+
+    if( toolID === ToolIDs.BEZIER )
+    {
+        document.getElementById( "toolName" ).innerText = show ? "Bezier Tool" : "";
+
+        document.getElementById( "showControlPolyCheck" ).style.visibility = visibility;
+        document.getElementById( "toolPointsScaleEdit" ).style.visibility = visibility;
+        document.getElementById( "toolPointsColorPicker" ).style.visibility = visibility;
+
+        const labels = document.getElementsByClassName( "bezier-labels" );
+        for( const label of labels )
+        {
+            label.style.visibility = visibility;
+        }
+    }
+    else if( toolID === ToolIDs.HERMITE )
+    {
+        document.getElementById( "toolName" ).innerText = show ? "Hermite Tool" : "";
+
+        document.getElementById( "showControlPolyCheck" ).style.visibility = visibility;
+        document.getElementById( "toolPointsScaleEdit" ).style.visibility = visibility;
+        document.getElementById( "toolPointsColorPicker" ).style.visibility = visibility;
+        document.getElementById( "toolVectorsScaleEdit" ).style.visibility = visibility;
+        document.getElementById( "toolVectorsColorPicker" ).style.visibility = visibility;
+
+        const labels = document.getElementsByClassName( "hermite-labels" );
+        for( const label of labels )
+        {
+            label.style.visibility = visibility;
+        }
+    }
+}
+
 export function makeToolsInactive()
 {
     document.getElementById( "bezierButton" ).style.backgroundColor = defaultButtonColor;
@@ -18,7 +54,8 @@ export function makeToolsInactive()
     document.getElementById( "addButton" ).style.backgroundColor = defaultButtonColor;
     document.getElementById( "deleteButton" ).style.backgroundColor = defaultButtonColor;
 
-    document.getElementById( "toolName" ).innerText = "";
+    showToolSettings( ToolIDs.BEZIER, false );
+    showToolSettings( ToolIDs.HERMITE, false );
 }
 
 export function makeToolActive( toolID )
@@ -26,12 +63,12 @@ export function makeToolActive( toolID )
     if( toolID === ToolIDs.BEZIER )
     {
         document.getElementById( "bezierButton" ).style.backgroundColor = activeButtonColor;
-        document.getElementById( "toolName" ).innerText = "Bezier Tool";
+        showToolSettings( ToolIDs.BEZIER );
     }
     else if( toolID === ToolIDs.HERMITE )
     {
         document.getElementById( "hermiteButton" ).style.backgroundColor = activeButtonColor;
-        document.getElementById( "toolName" ).innerText = "Hermite Tool";
+        showToolSettings( ToolIDs.HERMITE );
     }
     else if( toolID === ToolIDs.MOVE )
     {
