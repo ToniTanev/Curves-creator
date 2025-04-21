4aa704918fc6cea2f7c4521cf181c83720b23ac4
ToniTanev <toni.g.tanev@gmail.com>
2025-04-21 15:53:44 +0300

    Updating the comment for deleteObject() as it also deletes Line2 objects

--- MemoryManagement.js
+++ MemoryManagement.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import {scene} from "./CurveCreator.js";
 
-// deletes a Group or an Object3D
+// deletes a Group or an Object3D or a Line2 object
 export function deleteObject( object )
 {
     if( object )
